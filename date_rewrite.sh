f837c70157c047a74c40798ccf4a376c461d2dbd
John Higgins <johnmatthiggins@gmail.com>
2023-02-15 20:11:33 -0800

    Wrote more of the script

--- date_rewrite.sh
+++ date_rewrite.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
-echo ":pick e\n:wq" | git rebase "$1^" -i
-
-
+echo ":pick e\n:wq" | git rebase $1 -i
+GIT_COMMITTER_DATE="$2"
+git commit --amend --no-edit --date "$2"
+git rebase --continue

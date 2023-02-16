2f54fb6806a94f72e28c0619f8f749cfec532e0b
John Higgins <johnmatthiggins@gmail.com>
2023-02-15 20:16:04 -0800

    commit changes

--- date_rewrite.sh
+++ date_rewrite.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 echo "\x1B:wq\n" | git rebase $1 -i
+# echo "hello" | git rebase $1 -i
 # GIT_COMMITTER_DATE="$2"
 # git commit --amend --no-edit --date "$2"
 # git rebase --continue
